2f924f312b6e04738316784e7c375401cff680fe
98ping <67809373+98ping@users.noreply.github.com>
2023-10-05 22:46:28 -0700

    chore: revert to black canvas

--- login.js
+++ login.js
@@ -1,18 +1,17 @@
 var jwt = localStorage.getItem("jwt");
 if (jwt != null) {
-  window.location.href = './login.html'
+  window.location.href = './index.html'
 }
 
 function login() {
-  const email = document.getElementById("email").value;
+  const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
 
   const xhttp = new XMLHttpRequest();
-  xhttp.open("POST", "http://158.69.120.109:4223/user/login");
-  xhttp.setRequestHeader("Content-Type", "application/json");
-  xhttp.setRequestHeader("Authorize-API-Key", "AuthorizeKey12345")
+  xhttp.open("POST", "https://www.mecallapi.com/api/login");
+  xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
   xhttp.send(JSON.stringify({
-    "email": email,
+    "username": username,
     "password": password
   }));
   xhttp.onreadystatechange = function () {
@@ -20,19 +19,19 @@ function login() {
       const objects = JSON.parse(this.responseText);
       console.log(objects);
       if (objects['status'] == 'ok') {
-        localStorage.setItem("jwt", "test123");
+        localStorage.setItem("jwt", objects['accessToken']);
         Swal.fire({
-          text: objects[message],
+          text: objects['message'],
           icon: 'success',
           confirmButtonText: 'OK'
         }).then((result) => {
           if (result.isConfirmed) {
-            window.location.href = './login.html';
+            window.location.href = './index.html';
           }
         });
       } else {
         Swal.fire({
-          text: objects[message],
+          text: objects['message'],
           icon: 'error',
           confirmButtonText: 'OK'
         });
