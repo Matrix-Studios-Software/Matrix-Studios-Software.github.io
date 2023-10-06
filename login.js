9bae8007c6f512c0050ee08b294f133df4556fee
98ping <67809373+98ping@users.noreply.github.com>
2023-10-05 22:59:20 -0700

    fix: try and get the actual endpoint working

--- login.js
+++ login.js
@@ -1,26 +1,25 @@
-var jwt = localStorage.getItem("jwt");
-if (jwt != null) {
-  window.location.href = './index.html'
-}
+// var jwt = localStorage.getItem("jwt");
+// if (jwt != null) {
+//   window.location.href = './index.html'
+// }
 
 function login() {
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
 
   const xhttp = new XMLHttpRequest();
-  xhttp.open("POST", "https://www.melivecode.com/api/login");
+  xhttp.open("POST", "http://158.69.120.109:4223/user/login");
   xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  xhttp.setRequestHeader("Authorize-API-Key", "AuthorizeKey12345");
   xhttp.send(JSON.stringify({
     "username": username,
-    "password": password,
-    "expiresIn": 60000
+    "password": password
   }));
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4) {
       const objects = JSON.parse(this.responseText);
       console.log(objects);
       if (objects['status'] == 'ok') {
-        localStorage.setItem("jwt", objects['accessToken']);
         Swal.fire({
           text: objects['message'],
           icon: 'success',
