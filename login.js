cda3fe2d0fa711eb76ab50fbecbe40d4cb151e90
Max <67809373+98ping@users.noreply.github.com>
2023-10-07 23:16:18 -0700

    feat: add register page

--- login.js
+++ login.js
@@ -41,3 +41,42 @@ function login() {
   };
   return false;
 }
+
+function register() {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  const xhttp = new XMLHttpRequest();
+  xhttp.open("POST", "https://api.matrixstudios.ltd/user/register");
+  xhttp.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+  xhttp.setRequestHeader("Authorize-API-Key", "AuthorizeKey12345");
+  xhttp.send(JSON.stringify({
+    "email": username,
+    "password": password
+  }));
+
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4) {
+      const objects = JSON.parse(this.responseText);
+      console.log(objects);
+      if (objects['status'] == 'ok') {
+        Swal.fire({
+          text: objects['message'],
+          icon: 'success',
+          confirmButtonText: 'OK'
+        }).then((result) => {
+          if (result.isConfirmed) {
+            window.location.href = './index.html';
+          }
+        });
+      } else {
+        Swal.fire({
+          text: objects['message'],
+          icon: 'error',
+          confirmButtonText: 'OK'
+        });
+      }
+    }
+  };
+  return false;
+}
